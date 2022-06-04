87e8a812da0c1fb2e980891c9c348d3673b04c83
Gioi Giuseppe <giuseppe.gioi.1997@gmail.com>
2022-06-04 12:51:39 +0900

    Update CollisionMgr.cpp

--- Default/CollisionMgr.cpp
+++ Default/CollisionMgr.cpp
@@ -178,69 +178,6 @@ int CCollisionMgr::Collision_Sphere(list<CObj*> Sour, list<CObj*> Dest)
 	return iScore;
 }
 
-
-void CCollisionMgr::Collision_Wall(list<CObj*> Sour, list<CObj*> Wall)
-{
-	for (auto& iter = Sour.begin(); iter != Sour.end(); )
-	{
-		int iter_count = 0;
-		for (auto& Monster_iter = Wall.begin(); Monster_iter != Wall.end(); )
-		{
-			if ((Wall.size() != 0) && (IntersectRect(&rc, &((*iter)->Get_Rect()), &((*Monster_iter)->Get_Rect()))))
-			{
-				(*iter)->Set_Dead(true);
-				(*Monster_iter)->Set_Dead(true);
-			}
-			Monster_iter++;
-		}
-		iter++;
-	}
-}
-
-void CCollisionMgr::Collision_Wall_Player(list<CObj*> Sour, list<CObj*> Wall)
-{
-	for (auto& Player_iter = Sour.begin(); Player_iter != Sour.end(); Player_iter++)
-	{
-		for (auto& Wall_iter = Wall.begin(); Wall_iter != Wall.end(); Wall_iter++)
-		{
-			if ((Wall.size() != 0) && (IntersectRect(&rc, &((*Player_iter)->Get_Rect()), &((*Wall_iter)->Get_Rect()))))
-			{
-				float fWidth = float(rc.right - rc.left);
-				float fHeight = float(rc.bottom - rc.top);
-
-				if ((*Wall_iter)->Get_Dir() == DIR_RIGHT)
-					(*Player_iter)->Set_Pos((*Player_iter)->Get_Info().fX - fWidth, (*Player_iter)->Get_Info().fY);
-				if ((*Wall_iter)->Get_Dir() == DIR_LEFT)
-					(*Player_iter)->Set_Pos((*Player_iter)->Get_Info().fX + fWidth, (*Player_iter)->Get_Info().fY);
-				if ((*Wall_iter)->Get_Dir() == DIR_DOWN)
-					(*Player_iter)->Set_Pos((*Player_iter)->Get_Info().fX, (*Player_iter)->Get_Info().fY - fHeight);
-				if ((*Wall_iter)->Get_Dir() == DIR_UP)
-					(*Player_iter)->Set_Pos((*Player_iter)->Get_Info().fX, (*Player_iter)->Get_Info().fY + fHeight);
-			}
-		}
-	}
-
-int CCollisionMgr::Collision_Coin(CObj* Player, list<CObj*> Items)
-{
-	int iScore = 0;
-
-	for (auto& item : Items )
-	{
-		if (IntersectRect(&rc, &Player->Get_Rect(), &item->Get_Rect()))
-		{
-			CItem* _Item = dynamic_cast<CItem*>(item);
-
-			if (_Item->Get_Type() == ITEM_COIN)
-			{
-				iScore += 1;
-				_Item->Set_Dead(true);
-			}
-		}
-	}
-
-	return iScore;
-}
-
 bool CCollisionMgr::ChecK_Sphere(CObj* Sour, CObj* Dest)
 {
 	float fRadius = (Sour->Get_Info().fCX + Dest->Get_Info().fCX) * 0.5f; //두 원의 반지름을 더한 값
